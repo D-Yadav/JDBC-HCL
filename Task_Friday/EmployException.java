42d9191b87c8d34ebaffdac854fe8e42bb97481d
Deepak Yadav <52994272+D-Yadav@users.noreply.github.com>
2019-12-10 17:27:50 +0530

    Exception Handling In Employ Project

---
+++ Task_Friday/EmployException.java
@@ -0,0 +1,13 @@
+package Task_Friday;
+
+
+	public class EmployException extends Exception{
+		public EmployException() {
+			
+		}
+		public EmployException(String error) {
+			super(error);
+		}
+	}
+
+
